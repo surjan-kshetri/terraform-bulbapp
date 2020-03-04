1411c782b3f1a8353c991786ed9f0c0a37f360c1
Ubuntu <ubuntu@ip-172-31-82-19.ec2.internal>
2020-03-04 00:08:54 +0000

    Add

---
+++ task-definition.tf
@@ -0,0 +1,26 @@
+/*
+data "aws_ecs_task_definition" "default" {
+  task_definition = "${aws_ecs_task_definition.default.family}"
+}
+*/
+
+resource "aws_ecs_task_definition" "default" {
+    family                = "bulbapp"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "bulbapp",
+    "image": "747769733670.dkr.ecr.us-east-1.amazonaws.com/nginx-proxy:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 512,
+    "cpu": 1024
+  }
+]
+DEFINITION
+}
